1631e592388a89067c56cd95cad511ef8347f190
TanyaChas <tetiana2045@gmail.com>
2023-12-11 22:36:27 +0200

    HW_8_update

---
+++ src/test/java/Locators.java
@@ -0,0 +1,13 @@
+public class Locators {
+
+}
+WebElement title = driver.findElement(By.xpath("//title"));
+WebElement h1 = driver.findElement(By.xpath("//h1[1]"));
+WebElement img = driver.findElement(By.xpath("//img[1]"));
+WebElement ul = driver.findElement(By.xpath("//ul li:first-child(2)"));
+WebElement text = driver.findElement(By.xpath("//input[type=\"text\"]"));
+WebElement text = driver.findElement(By.xpath("//a[contains(text(), \"Clojure\"]"));
+WebElement text = driver.findElement(By.xpath("//input[@value=\"Search workflows\"]"));
+WebElement text = driver.findElement(By.xpath("//img[@class='CircleBadge-icon'][3]"));
+WebElement text = driver.findElement(By.xpath("//a[@class='btn btn-sm'][1]"));
+WebElement text = driver.findElement(By.xpath("//h2[contains(text(), \"Security\"]"));
